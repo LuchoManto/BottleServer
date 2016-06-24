67ef32c25cd09a036058a5a2d97b2657e11c5e3f
root <root@raspberrypi.(none)>
2016-06-24 19:43:14 +0000

    funcionando con dos veces el ST

--- helpers/serial_uart.py
+++ helpers/serial_uart.py
@@ -24,13 +24,14 @@ class ClaseSerial:
         self.sched = BackgroundScheduler()
         self.buffer_mediciones = collections.deque(maxlen=20)
         self.e = threading.Event()
+	self.st1 = 0
+	self.waitt = 0
+	self.waitt2 = 0
 
     def enviarYObtenerRespuesta(self,toSend):
         #self.port.Open()
         self.port.flushInput()
         self.port.flushOutput()
-	if (toSend == 's'):
-	    self.e.wait()
         self.port.write(str(toSend) + "\n")
         time.sleep(1)
 	self.port.flushOutput()
@@ -53,29 +54,40 @@ class ClaseSerial:
         recv = self.enviarYObtenerRespuesta(send)
         cargar_comand_log(send, recv)
         #todo: save in db: recv, in command log table
-    def kill_thread(self):
-	t1.exit()
-	t2.exit()
+    
+    def kill_threads(self):
+	
+	self.waitt = 1
+	self.waitt2= 1
+	#self.e.wait()
+	#self.e1.wait()
+	#cargar_comand_log('wait', '1')
 
     def start_conversion_ST(self):
-        try:
-            # thread.start_new_thread(self.keepGoing_start())
-            t1 = threading.Thread(name='producer',
+        if self.waitt == 1 and self.waitt2 == 1:
+	    #self.st1 = 0
+	    self.e.set()
+	else:
+	    self.st1 = 1
+            try:
+            	# thread.start_new_thread(self.keepGoing_start())
+            	t1 = threading.Thread(name='producer',
                                   target=self.loop_productor,
-                                  args=[])
-            t1.start()
+                                  args=(self.e,))
+            	t1.start()
 
-        except:
-            print "Error: unable to start thread"
+            except:
+            	print "Error: unable to start thread-producer"
 
-        try:
-            t2 = threading.Thread(name='consumer',
+            try:
+            	t2 = threading.Thread(name='consumer',
                                   target=self.loop_consumidor,
-                                  args=[])
-            t2.start()
-        except:
-            print "Error: unable to start thread"
+                                  args=(self.e,))
+            	t2.start()
+            except:
+            	print "Error: unable to start thread-consumer"
 
+         
     def triggerStart(self):
         print "disparo"
 
@@ -129,10 +141,10 @@ class ClaseSerial:
         send = "NTP"
         recv = self.enviarYObtenerRespuesta(send)
         cargar_comand_log(send, recv)
-	        #todo: save in db: recv, in command log table
+	#todo: save in db: recv, in command log table
 
     #thread que recibe los datos desde la uart y los guarda en el buffer. PRODUCTOR
-    def loop_productor(self):
+    def loop_productor(self, e):
 
         #todo: guardar en la base de datos: "producer thread started", en la tabla de log del monitor
         #todo: vaciar buffer
@@ -141,16 +153,19 @@ class ClaseSerial:
         #todo: guardar en la base de datos: "consumer thread started", en la tabla de log del monitor
 
         while 1:
-            toSave = self.recibir()
+            #toSave = self.recibir()
 	    #toSave3 = retrieve_conversion(toSave)
-	    if toSave == '':
-                cargar_comand_log('Producer ended', 03000)
-                #self.e.wait()
-                break
-            else:
-                self.buffer_mediciones.append(toSave)
+            if self.waitt2 == 1:
+		cargar_comand_log('procesor ended','1')
+		self.waitt2 = 0
+		#self.e.wait()
+		break
+	    #else:
+	    toSave = self.recibir()
+	    self.buffer_mediciones.append(toSave)
+	    self.e.set()
             #toSave3 = retrieve_conversion(toSave)
-            self.e.set()
+            #self.e.set()
 
         #loop forever:
         #guardar en buffer
@@ -168,24 +183,29 @@ class ClaseSerial:
 
 
 
-    def loop_consumidor(self): #thread que guarda los datos leidos del buffer en la base de datos. CONSUMIDOR
+    def loop_consumidor(self, e): #thread que guarda los datos leidos del buffer en la base de datos. CONSUMIDOR
         last = "0"
         while 1:
             if not self.buffer_mediciones:
-                if self.keepGoing == 0:
+            	if self.keepGoing == 0:
                     cargar_comand_log('consumer thread ended', '0')
                     #todo: guardar "termino thread consumidor" en la db, en la tabla de log
                     self.e.wait()
 		    break
-                else:
-                    self.e.wait()
+                elif self.waitt == 1:
+		    self.waitt=0
+                    cargar_comand_log('wait primer consu', '3')
+		    self.e.wait()
                     #todo: sleep hasta que haya algo en el buffer
             else:
-                toSave=self.buffer_mediciones.popleft()
-		if toSave == '':
-                    break
-		else:
-                    toSave1 = retrieve_conversion(toSave)
+                toSave=self.buffer_mediciones.popleft()		
+		if self.waitt == 1:
+		    cargar_comand_log('wait consu','2')
+		    self.waitt =0
+		    #self.e.wait()
+		    break
+                else:
+		    toSave1 = retrieve_conversion(toSave)
 		    current = retrieve_timestamp(toSave)
                     base = get_pi_timestamp_ms()
                     tstamp = generate_timestamp(current, last, base)
