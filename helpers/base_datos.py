527a213e690a81a0a69c593688b3545a21ca265e
ignaciosamba <ignaciosamba@gmail.com>
2017-04-04 21:54:20 -0300

    ,

--- helpers/base_datos.py
+++ helpers/base_datos.py
@@ -32,7 +32,7 @@ def cargar_desde_bd_medicion():
     global row_numb_medicion
     db = MySQLdb.connect("localhost", "ignacio", "mantosamba", "SensorCampoElectroEstatico")
     curs = db.cursor()
-    curs.execute("SELECT * FROM medicion LIMIT row_numb_medicion999999999999999")
+    curs.execute("SELECT * FROM medicion LIMIT row_numb_medicion,999999999999999")
     row_numb_medicion = curs.execute("SELECT COUNT(*) FROM medicion")
     for (hora, pin, medicion) in curs:
         dato_medicion = Dato_db(hora, pin, medicion)
@@ -45,7 +45,7 @@ def cargar_desde_bd_comando():
     global row_numb_log
     db = MySQLdb.connect("localhost", "ignacio", "mantosamba", "SensorCampoElectroEstatico")
     curs = db.cursor()
-    curs.execute("SELECT * FROM comandlog LIMIT row_numb_log, 999999999999999")
+    curs.execute("SELECT * FROM comandlog LIMIT row_numb_log,999999999999999")
     row_numb_log = curs.execute("SELECT COUNT(*) FROM comandlog")
     for (fecha, hora, comando, respuesta) in curs:
         dato_comando = Dato_db_log(fecha, hora, comando, respuesta)
